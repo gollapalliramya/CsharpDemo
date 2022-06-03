ef6d0d268ead55def859022f7608d1a8343d0487
gollapalliramya <105625326+gollapalliramya@users.noreply.github.com>
2022-06-03 14:56:29 +0530

    initial commit

---
+++ Tuple feild Names.cs
@@ -0,0 +1,5 @@
+var t = (Sum: 4.5, Count: 3);
+Console.WriteLine($"Sum of {t.Count} elements is {t.Sum}.");
+
+(double Sum, int Count) d = (4.5, 3);
+Console.WriteLine($"Sum of {d.Count} elements is {d.Sum}.");
